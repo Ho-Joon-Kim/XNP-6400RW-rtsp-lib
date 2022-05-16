48cebb990b030e3bed75b0d49cc9380f037ec6d1
TitanV <hojoon.dev@gmail.com>
2022-05-17 01:09:50 +0900

    add - source code

---
+++ README.md
@@ -0,0 +1,14 @@
+라이브러리라고 하기에는 뭣하지만 xnp 시리즈 카메라의 rtsp 라우트가 메뉴얼에 쓰여있지 않아 만들어놓는 아카이브......
+
+rtsp://id:pw@ip:554/profile2/media.smp
+
+
+``` python
+from src/index import xnp_rtsp
+
+test = xnp_rtsp()
+
+#location is optional
+test.init("rtsp://id:pw@ip:554/profile2/media.smp", "/location")
+test.takeImage()
+```
---
+++ src/index.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import shutil
+import uuid
+import ffmpeg
+
+class xnp_rtsp:
+    def __new__(cls):
+        if not hasattr(cls,'instance'):
+            cls.instance = super(xnp_rtsp, cls).__new__(cls)
+        return cls.instance
+    
+    def __init__(self, RTSP_URL, fileLocation = ""):
+        self.RTSP_URL = RTSP_URL
+        self.fileLocation = fileLocation
+
+    def takeImage(self):
+        fileName = datetime.now().strftime("%Y%m%d%H%M%S") + "-" + str(uuid.uuid4()) + ".png"
+        stream = ffmpeg.input(self.RTSP_URL, ss=0)
+        file = stream.output(fileName, vframes=1)
+        file.run(capture_stdout=True, capture_stderr=True)
+
+        if self.fileLocation != "":
+            shutil.move(fileName, self.fileLocation + "/" + fileName)
+        return fileName
